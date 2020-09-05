de404de49b2df7e97abd69d14658c8dfee1d53d0
tutkuince <tutku.ince@outlook.com>
2020-09-05 16:00:49 +0300

    substitution test

--- src/main/java/com/muditasoft/artofwritingunittest/sample01/Calculator.java
+++ src/main/java/com/muditasoft/artofwritingunittest/sample01/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public int addition(int number1, int number2) {
         return number1 + number2;
     }
+
+    public int substitution(int number1, int number2) {
+        return number1 - number2;
+    }
 }
--- src/test/java/com/muditasoft/artofwritingunittest/sample01/CalculatorTest.java
+++ src/test/java/com/muditasoft/artofwritingunittest/sample01/CalculatorTest.java
@@ -1,22 +1,44 @@
 package com.muditasoft.artofwritingunittest.sample01;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     void test_addition() {
-        // on hazirlik
-        Calculator calculator = new Calculator();
+        // GIVEN
         int number1 = 5;
         int number2 = 19;
 
-        // methodu calistir
+        // WHEN
         final int addition = calculator.addition(number1, number2);
 
-        // sonucu kontrol et
+        // THEN
         assertEquals(24, addition);
+    }
+
+    @Test
+    void test_substitution() {
+        assertEquals(9, calculator.substitution(19, 10));
+    }
+
+    @Test
+    void when_number_substitution_from_zero_expect_negative_number() {
+        assertEquals(-19, calculator.substitution(0, 19));
+    }
+
+    @Test
+    void when_number_substitution_from_zero_number_expect_number_is_same_as_before() {
+        assertEquals(10, calculator.substitution(10, 0));
     }
 }
