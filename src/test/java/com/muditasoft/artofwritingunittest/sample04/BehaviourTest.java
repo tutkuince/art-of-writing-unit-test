b28fc16cffd0c2fe0b9513dca7778d12a8467555
tutkuince <tutku.ince@outlook.com>
2020-09-09 21:57:49 +0300

    test_how_many_times_called_that_methods

--- src/test/java/com/muditasoft/artofwritingunittest/sample04/BehaviourTest.java
+++ src/test/java/com/muditasoft/artofwritingunittest/sample04/BehaviourTest.java
@@ -3,7 +3,7 @@ package com.muditasoft.artofwritingunittest.sample04;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class BehaviourTest {
 
@@ -16,5 +16,23 @@ public class BehaviourTest {
 
         verify(dummyCustomerService).addCustomer("Istanbul");
         verify(dummyCustomerService).addCustomer("Izmir");
+    }
+
+    @Test
+    void test_how_many_times_called_that_methods() {
+        DummyCustomerService dummyCustomerService = Mockito.mock(DummyCustomerService.class);
+
+        dummyCustomerService.addCustomer("Istanbul");
+        dummyCustomerService.addCustomer("Izmir");
+        dummyCustomerService.addCustomer("Izmir");
+        dummyCustomerService.addCustomer("Izmir");
+
+        verify(dummyCustomerService, times(3)).addCustomer("Izmir");
+        verify(dummyCustomerService, times(1)).addCustomer("Istanbul");
+
+        verify(dummyCustomerService, never()).removeCustomer(anyString());
+        verify(dummyCustomerService, never()).addCustomer("Ankara");
+
+        verify(dummyCustomerService, atLeast(1)).addCustomer("Istanbul");
     }
 }
