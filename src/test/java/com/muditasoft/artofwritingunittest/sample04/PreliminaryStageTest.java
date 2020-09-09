34d56b86c6bdf0c168d0890de498a3d058b942e2
tutkuince <tutku.ince@outlook.com>
2020-09-09 22:16:02 +0300

    test_when_then_usage_for_void_return_types

--- src/test/java/com/muditasoft/artofwritingunittest/sample04/PreliminaryStageTest.java
+++ src/test/java/com/muditasoft/artofwritingunittest/sample04/PreliminaryStageTest.java
@@ -6,8 +6,7 @@ import org.mockito.Mockito;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class PreliminaryStageTest {
 
@@ -28,5 +27,15 @@ public class PreliminaryStageTest {
         when(customerService.getCustomer("ankara")).thenThrow(new IllegalArgumentException("EXCEPTION"));
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> customerService.getCustomer("ankara"));
+    }
+
+    @Test
+    void test_when_then_usage_for_void_return_types() {
+        DummyCustomerService customerService = mock(DummyCustomerService.class);
+
+        doNothing().when(customerService).addCustomer(anyString());
+
+        doReturn("ankara").when(customerService).getCustomer("Izmir");
+
     }
 }
