45c667877b1359218a9319b54ff0a845647acfca
tutkuince <tutku.ince@outlook.com>
2020-09-09 22:52:10 +0300

    deleted redundant imports

--- src/test/java/com/muditasoft/artofwritingunittest/sample04/AnnotationTest.java
+++ src/test/java/com/muditasoft/artofwritingunittest/sample04/AnnotationTest.java
@@ -4,15 +4,11 @@ import com.muditasoft.artofwritingunittest.sample02.model.Customer;
 import com.muditasoft.artofwritingunittest.sample02.repository.CustomerRepository;
 import com.muditasoft.artofwritingunittest.sample02.service.CustomerService;
 import com.muditasoft.artofwritingunittest.sample02.service.InformationService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class AnnotationTest {
